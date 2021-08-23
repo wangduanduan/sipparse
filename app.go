d238d132f835f34f93758f5d1bb2b8816d035a2d
wangdd <wangdd@dd.com>
2021-08-23 22:58:56 +0800

    WIP

--- app.go
+++ app.go
@@ -1,38 +1,56 @@
 package sipparse
 
-const (
-	MInvite    = "INVITE"
-	MCancel    = "CANCEL"
-	MAck       = "ACK"
-	MBye       = "BYE"
-	MInfo      = "INFO"
-	MOptions   = "OPTIONS"
-	MUpdate    = "UPDATE"
-	MRegister  = "REGISTER"
-	MMessage   = "MESSAGE"
-	MSubscribe = "SUBSCRIBE"
-	MNotify    = "NOTIFY"
-	MPrack     = "PRACK"
-	MRefer     = "REFER"
-	MPublish   = "PUBLISH"
-)
+var empty = struct{}{}
+var acceptMethods map[string]struct{}
+var discardMethods map[string]struct{}
 
-type SIPURI struct {
-	user   string
-	domain string
+type SIP struct {
+	Title           string // Method or Status
+	IsRequest       bool
+	CallID          string
+	RequestUsername string
+	RequestDomain   string
+	ToUsername      string
+	ToDomain        string
+	FromUsername    string
+	FromDomain      string
+	CseqNumber      int
+	CseqMethod      string
+	SrcAddr         string // IP:PORT
+	DstAddr         string // IP:PORT
+	CreateAt        string
+	Protocol        int
+	raw             *string // raw sip message
 }
 
-type SIPCSEQ struct {
-	num    int
-	method string
-}
+func init() {
+	am := map[string]struct{}{
+		"INVITE":    empty,
+		"CANCEL":    empty,
+		"ACK":       empty,
+		"BYE":       empty,
+		"INFO":      empty,
+		"OPTIONS":   empty,
+		"UPDATE":    empty,
+		"REGISTER":  empty,
+		"MESSAGE":   empty,
+		"SUBSCRIBE": empty,
+		"NOTIFY":    empty,
+		"PRACK":     empty,
+		"REFER":     empty,
+		"PUBLISH":   empty,
+	}
 
-type SIPMSG struct {
-	IsRequest  bool
-	CallID     string
-	RequestURL SIPURI
-	FromURL    SIPURI
-	ToRUL      SIPURI
-	CSEQ       SIPCSEQ
-	raw        string
+	// may be read from env
+	dm := map[string]struct{}{
+		"INFO":      empty,
+		"OPTIONS":   empty,
+		"REGISTER":  empty,
+		"MESSAGE":   empty,
+		"SUBSCRIBE": empty,
+		"PUBLISH":   empty,
+	}
+
+	acceptMethods = am
+	discardMethods = dm
 }
